242ea43d1ecb1062b2fcb41061f424778f98b6f3
Dmytro Davydov <dmytro.davydov@cheetahdigital.com>
2024-02-06 17:23:32 +0000

    mui charts - out, recharts- in

--- src/components/life-strategy/Chart.jsx
+++ src/components/life-strategy/Chart.jsx
@@ -1,20 +1,55 @@
 import * as React from 'react';
-import { BarChart } from '@mui/x-charts';
+import {
+    ScatterChart,
+    Scatter,
+    XAxis,
+    YAxis,
+    ZAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+} from 'recharts';
+
+const data01 = [
+    { x: 100, y: 200, z: 200 },
+    { x: 120, y: 100, z: 260 },
+    { x: 170, y: 300, z: 400 },
+    { x: 140, y: 250, z: 280 },
+    { x: 150, y: 400, z: 500 },
+    { x: 110, y: 280, z: 200 },
+];
+const data02 = [
+    { x: 200, y: 260, z: 240 },
+    { x: 240, y: 290, z: 220 },
+    { x: 190, y: 290, z: 250 },
+    { x: 198, y: 250, z: 210 },
+    { x: 180, y: 280, z: 260 },
+    { x: 210, y: 220, z: 230 },
+];
 
 const Chart = () => {
-  return (
-    <BarChart
-      series={[
-        { data: [35, 44, 24, 34] },
-        { data: [51, 6, 49, 30] },
-        { data: [15, 25, 30, 50] },
-        { data: [60, 50, 15, 25] },
-      ]}
-      height={290}
-      xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
-      margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
-    />
-  );
-}
+    return (
+        <ResponsiveContainer width="100%" height={400}>
+            <ScatterChart
+                margin={{
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+                }}
+            >
+            <CartesianGrid />
+            <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+            <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+            <ZAxis type="number" dataKey="z" range={[60, 400]} name="score" unit="km" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Legend />
+            <Scatter name="A school" data={data01} fill="#8884d8" shape="star" />
+            <Scatter name="B school" data={data02} fill="#82ca9d" shape="triangle" />
+          </ScatterChart>
+        </ResponsiveContainer>
+    );
+};
 
 export default Chart;
