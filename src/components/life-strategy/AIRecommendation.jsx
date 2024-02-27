d127d0563c2da4ac6e7bb423214f6c9c82945ad5
Dmytro Davydov <dmytro.davydov@cheetahdigital.com>
2024-02-27 21:50:13 +0000

    openAI api call added

--- src/components/life-strategy/AIRecommendation.jsx
+++ src/components/life-strategy/AIRecommendation.jsx
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Box, Paper, Typography, Accordion, AccordionSummary, AccordionDetails,
-    List, ListItem, ListItemText, Stack} from '@mui/material';
+    List, ListItem, ListItemText, ListItemButton, Stack} from '@mui/material';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { styled } from '@mui/material/styles';
 import { LifeStrategyContext } from '../../context';
+import useDialog from '../../hooks/useDialog';
 
 const StyledListItem = styled(ListItem)(({ theme }) => ({
     color: theme.palette.text.darkBlue,
@@ -11,6 +12,54 @@ const StyledListItem = styled(ListItem)(({ theme }) => ({
 
 const AIRecommendation = () => {
     const { state, setState } = useContext(LifeStrategyContext);
+    const [ DialogComponent, openDialog ] = useDialog();
+
+    const [recommendation, setRecommendation] = useState('No recommendation. You are perfect!');
+
+    console.log('AIRecommendation:::state:::', state);
+
+    const url = "https://api.openai.com/v1/chat/completions";
+
+    const sysMsg = `You are a personal life coach. 
+    In a conversational professional tone, based on the [User Data] provide a recommendation
+    as to what actions to take in order to get a good and balanced life across all areas.`;
+
+    const prompt = `User Data: ${JSON.stringify(state)}`;
+
+    const data = {
+        model: "gpt-4-0613",
+        messages: [
+            { role: "system", content: sysMsg },
+            { role: "user", content: prompt },
+        ],
+    };
+
+    const params = {
+        headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_OPENAI}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        method: "POST",
+    };
+
+    useEffect(() => {
+        if (!state.showAIRecommendation) {
+            return;
+        }
+        fetch(url, params)
+            .then(response => response.json())
+            .then(data => {
+                // return data.choices[0].message.content
+                console.log('AIRecommendation:::useEffect:::data:::', data);
+                setRecommendation(data.choices[0].message.content);
+            })
+            .catch(error => {
+            console.log("Error:", error);
+            return Promise.reject(
+                "Ooops! Smth's wrong :/ Please, try again later."
+            )});
+    }, [state.showAIRecommendation]);
 
     const handleClick = () => {
         setState({ ...state,
@@ -33,25 +82,19 @@ const AIRecommendation = () => {
                 </AccordionSummary>
                 <AccordionDetails>
                     <List dense>
-                        Here will be the AI recommendation
-                        {/* <StyledListItem>
+                        <ListItem>
                             <Stack spacing={0}>
-                                <ListItemText primary="Strong areas"/>
+                                <ListItemButton onClick={() => openDialog({
+                                    content: recommendation
+                                })}>
+                                    <ListItemText primary='Make it bigger'/>
+                                </ListItemButton>
                             </Stack>
-                        </StyledListItem>
-                        <StyledListItem>
-                            <Stack spacing={0}>
-                                <ListItemText primary="Weak areas"/>
-                            </Stack>
-                        </StyledListItem>
-                        <StyledListItem>
-                            <Stack spacing={0}>
-                                <ListItemText primary="Strategy"/>
-                            </Stack>
-                        </StyledListItem> */}
+                        </ListItem>
                     </List>
                 </AccordionDetails>
             </Accordion>
+            <DialogComponent />
         </Box>
     );
 };
